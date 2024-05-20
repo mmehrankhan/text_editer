f56444eed981c645403642e1bbd1ee3d8395bdf6
M_Mehran <154292817+mmehrankhan@users.noreply.github.com>
2024-05-20 10:22:20 -0700

    text editer

---
+++ app.js
@@ -0,0 +1,26 @@
+function myFunction(){
+    document.getElementById("textarea").style.fontWeight= 'bold';
+    
+    }
+    
+function myFunction1(){
+    
+    document.getElementById("textarea").style.fontStyle= 'italic';
+    
+    }
+    
+function myFunction2()
+    
+{
+    document.getElementById("textarea").style.textDecorationLine= 'underline';
+
+}
+function myFunction3() {
+    document.getElementById("textarea").style.listStyleType= 'square';
+
+}
+
+
+function myFunction4(){
+    document.getElementById("textarea").style.listStyleType='circle';
+}

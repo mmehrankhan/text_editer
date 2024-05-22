d475ac5d49f4f7d7f8ada728d267a0aa7a59cb7b
M_Mehran <154292817+mmehrankhan@users.noreply.github.com>
2024-05-22 10:10:42 -0700

    text

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
-function myFunction(){
-    document.getElementById("textarea").style.fontWeight= 'bold';
-    
+
+
+var text = document.getElementById('text')
+var btns = document.getElementsByClassName('action_btn')
+console.log(btns);
+var fontSize = document.getElementById('font_size')
+
+fontSize.addEventListener('change' , function(){
+    console.log(this.value);
+    if(this.value === 'heading'){
+        text.style.fontSize = '41px'
     }
-    
-function myFunction1(){
-    
-    document.getElementById("textarea").style.fontStyle= 'italic';
-    
+    if(this.value === 'subheading'){
+        text.style.fontSize = '25px'
     }
-    
-function myFunction2()
-    
-{
-    document.getElementById("textarea").style.textDecorationLine= 'underline';
+    if(this.value === 'normal'){
+        text.style.fontSize = '16px'
+    }
+})
 
+for (var i=0; i<btns.length; i++){
+    btns[i].addEventListener('click' , function() {
+        console.log('this==>'+ this.innerText);
+        switch (this.innerText) {
+            case 'bold':
+                if (text.style.fontWeight == 'bold') {
+                    text.style.fontWeight = 'normal' 
+                }
+                else{
+                    text.style.fontWeight = 'bold'
+                }
+                break;
+            case 'italic':
+                if (text.style.fontStyle == 'italic') {
+                    text.style.fontStyle = 'normal' 
+                }
+                else{
+                    text.style.fontStyle = 'italic'
+                }                
+                break;
+            case 'underline':
+                if (text.style.textDecorationLine == 'underline') {
+                    text.style.textDecorationLine = 'none' 
+                }
+                else{
+                    text.style.textDecorationLine = 'underline'
+                }
+                break;
+            case 'right':
+                text.style.textAlign = 'right'
+
+                break;
+            case 'left':
+                text.style.textAlign = 'left'
+
+                break;
+            case 'center':
+                text.style.textAlign = 'center'
+
+                break;        
+            default:
+                break;
+        }
+    })
 }
-function myFunction3() {
-    document.getElementById("textarea").style.listStyleType= 'square';
 
-}
-
-
-function myFunction4(){
-    document.getElementById("textarea").style.listStyleType='circle';
-}
